15ad5476ed7901f9e6aa8b198a39fee1f3e7d86d
ABEERANOOR2202 <abeeranoor2202@gmail.com>
2026-04-12 00:13:43 +0500

    removed unnecessary file

--- hash_password.py
+++
@@ -1,8 +0,0 @@
-import bcrypt
-
-# compute hash for given password
-def hash_password(pw):
-    return bcrypt.hashpw(pw.encode(), bcrypt.gensalt()).decode()
-
-if __name__ == '__main__':
-    print(hash_password('12345678'))
