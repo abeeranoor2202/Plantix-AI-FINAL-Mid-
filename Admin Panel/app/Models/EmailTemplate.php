45aee074273e8643f94e9d1778ba77b08a877201
ABEERANOOR2202 <abeeranoor2202@gmail.com>
2026-04-14 01:54:58 +0500

    feat: Update EmailTemplate model to include additional fillable fields

--- Admin Panel/app/Models/EmailTemplate.php
+++ Admin Panel/app/Models/EmailTemplate.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class EmailTemplate extends Model
 {
-    protected $fillable = ['type', 'subject', 'body', 'is_active', 'firebase_doc_id'];
+    protected $fillable = ['name', 'type', 'email_type', 'subject', 'body', 'variables', 'is_active', 'firebase_doc_id', 'is_send_to_admin'];
 
     protected $casts = [
         'is_active' => 'boolean',
