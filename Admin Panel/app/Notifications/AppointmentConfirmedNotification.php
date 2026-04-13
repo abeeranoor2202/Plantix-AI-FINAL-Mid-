7674ea4b5e692d40f0136393b9108aa780511882
ABEERANOOR2202 <abeeranoor2202@gmail.com>
2026-04-14 02:02:03 +0500

    fix: Remove database notification channel from AppointmentConfirmedNotification

--- Admin Panel/app/Notifications/AppointmentConfirmedNotification.php
+++ Admin Panel/app/Notifications/AppointmentConfirmedNotification.php
@@ -23,7 +23,7 @@ class AppointmentConfirmedNotification extends Notification implements ShouldQue
 
     public function via(object $notifiable): array
     {
-        return ['mail', 'database'];
+        return ['mail'];
     }
 
     public function toMail(object $notifiable): MailMessage
