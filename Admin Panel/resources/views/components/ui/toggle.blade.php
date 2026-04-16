a89deba65675758c9bd62abfa5e2c1800ad17dba
ABEERANOOR2202 <abeeranoor2202@gmail.com>
2026-04-17 01:32:04 +0500

    feat: Remove unused UI component files for badge, button, table, and toggle

--- Admin Panel/resources/views/components/ui/toggle.blade.php
+++
@@ -1,9 +0,0 @@
-@props([
-    'checked' => false,
-    'name' => 'enabled',
-])
-
-<label class="switch">
-    <input type="checkbox" name="{{ $name }}" {{ $checked ? 'checked' : '' }} {{ $attributes }}>
-    <span class="slider"></span>
-</label>
