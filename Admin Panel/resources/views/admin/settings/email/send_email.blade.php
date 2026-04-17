ced8f4883fcbca9b98017d7d87b9a119158a6ca6
ABEERANOOR2202 <abeeranoor2202@gmail.com>
2026-04-18 03:22:57 +0500

    feat: Refactor email template to extend master layout and improve content structure

--- Admin Panel/resources/views/admin/settings/email/send_email.blade.php
+++ Admin Panel/resources/views/admin/settings/email/send_email.blade.php
@@ -1,14 +1,25 @@
-<html>
-<head>
+@extends('emails.layouts.master', [
+    'heroIcon' => '📢',
+    'heroTitle' => $emailSubject ?? 'Platform Announcement',
+    'heroSubtitle' => config('app.name') . ' admin communication',
+    'emailSubject' => $emailSubject ?? 'Platform Announcement',
+])
 
-    <title></title>
-</head>
-<body>
+@section('content')
+@php
+    $safeMessage = strip_tags((string) ($messageBody ?? ''), '<p><br><strong><b><em><i><ul><ol><li><a>');
+@endphp
 
-{!!$data !!}
+<p>Hi,</p>
 
-</body>
-</html>
+<div style="line-height:1.7; color:#334155;">
+    {!! $safeMessage !!}
+</div>
+
+<div class="alert-box alert-info">
+    This message was sent by a platform administrator.
+</div>
+@endsection
 
 
 
